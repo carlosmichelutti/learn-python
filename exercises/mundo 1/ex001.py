53ff58f66a7d3d6b58a1fbed3a50a11e06edf3b0
Carlos Michelutti <carlos.stz590@gmail.com>
2025-07-09 22:27:25 -0300

    Update

--- exercises/mundo 1/ex001.py
+++ exercises/mundo 1/ex001.py
@@ -2,4 +2,4 @@ numero1 = int(input('Digite um valor: '))
 numero2 = int(input('Digite outro valor: '))
 soma = numero1 + numero2
 
-print(f'A soma entre {numero1} e {numero2} é igual a {soma}.')
+print(f'A soma dos números {numero1} e {numero2} é igual a {soma}.')
