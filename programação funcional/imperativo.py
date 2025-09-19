c843af0dbca53036006dc1dc041ff256d8d93dc3
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-19 14:53:14 -0300

    Delete imperativo.py

--- programação funcional/imperativo.py
+++
@@ -1,9 +0,0 @@
-from calendar import mdays, month_name
-from os import system
-
-system('cls')
-
-print('MESES COM 31 DIAS')
-for mes in range(1, 13):
-    if mdays[mes] == 31:
-        print(f'- {month_name[mes]}')
