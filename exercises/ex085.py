79fa38e5599bbf5ded481cd98a8e2cb44f57d15c
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-19 14:52:28 -0300

    Create ex085.py

---
+++ exercises/ex085.py
@@ -0,0 +1,13 @@
+def cores_do_arco_iris():
+    yield 'vermelho'
+    yield 'laranja'
+    yield 'amarelo'
+    yield 'verde'
+    yield 'azul'
+    yield 'índido'
+    yield 'violeta'
+
+generator = cores_do_arco_iris()
+for cor in generator:
+    print(cor)  
+    
