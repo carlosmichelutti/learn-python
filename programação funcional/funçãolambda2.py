276ae73b0753806633ed52f5c5e75570d63e9647
Carlos Michelutti <carlos.stz590@gmail.com>
2025-09-19 14:52:59 -0300

    Delete funçãolambda2.py

--- programação funcional/funçãolambda2.py
+++
@@ -1,8 +0,0 @@
-products = ({'quantidade': 10, 'Preço': 10},
-            {'quantidade': 20, 'Preço': 20},
-            {'quantidade': 5, 'Preço': 100})
-
-soma_total_de_cada_produto = list(map(lambda products: products['quantidade'] * products['Preço'], products))
-
-print(f'Preços totais: {soma_total_de_cada_produto}')
-print(f'Preço total total: {sum(soma_total_de_cada_produto)}')
