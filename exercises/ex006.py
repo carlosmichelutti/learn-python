efc003a397024a39d9601e0a2b4fe2e1e5ddd360
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 20:32:19 -0300

    Update ex006.py

--- exercises/ex006.py
+++ exercises/ex006.py
@@ -1,7 +1,7 @@
-carteira = float(input('Quanto de dinheiro você tem na sua carteira? R$'))
+wallet_amount = float(input('How much money do you have in your wallet? R$'))
 
-dolares = carteira / 5.21
-euros = carteira / 5.67
-ienes =  carteira * 25.01
+dollars = wallet_amount / 5.21
+euros = wallet_amount / 5.67
+yen = wallet_amount * 25.01
 
-print(f'Com R${carteira} você tem U${dolares:.2F} dolares, €{euros:.2f} euros e ¥{ienes:.2f} ienes.')
+print(f'With R${wallet_amount} you have US${dollars:.2f} dollars, €{euros:.2f} euros and ¥{yen:.2f} yen.')
