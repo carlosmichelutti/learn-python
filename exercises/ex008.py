f869f3fad0f40f13098c99f92ebdd153320a81a0
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 19:50:56 -0300

    Update ex008.py

--- exercises/ex008.py
+++ exercises/ex008.py
@@ -2,4 +2,4 @@ valor = float(input('Qual o valor do produto? R$'))
 
 desconto = valor * 0.9
 
-print(f'O produto que custava R${valor:.2f}, na promoção de 10% ele estará custando R${desconto:.2f}.')
+print(f'O produto que custava R${valor:.2f} na promoção de 10% ele estará custando R${desconto:.2f}.')
