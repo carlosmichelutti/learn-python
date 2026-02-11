139ca92b9d8b16f134a5c6f6a5d4332fc1e49f8c
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 19:50:58 -0300

    Update ex009.py

--- exercises/ex009.py
+++ exercises/ex009.py
@@ -2,4 +2,4 @@ salario = float(input('Quanto é o seu salário? R$'))
 
 aumento = salario * 1.15
 
-print(f'Uma pessoa que recebe recebe R${salario}, com um aumento de 15% passará a ganhar R${aumento:.2f}.')
+print(f'Uma pessoa que recebe recebe R${salario} com um aumento de 15% passará a ganhar R${aumento:.2f}.')
