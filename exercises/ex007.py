adf1bb16ffedaca32bb1b0bfaa0aca41f0135ef8
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 20:32:22 -0300

    Update ex007.py

--- exercises/ex007.py
+++ exercises/ex007.py
@@ -1,8 +1,8 @@
-comprimento = float(input('Digite o comprimento da parede: '))
-largura = float(input('Digite a largura da parede: '))
+length = float(input('Enter the wall length: '))
+width = float(input('Enter the wall width: '))
 
-area = comprimento * largura
-tinta_necessaria = area / 2
+area = length * width
+paint_needed = area / 2
 
-print(f'Sua parede tem a dimensão {comprimento}x{largura} e a sua área é de {area}m².')
-print(f'A sua parede de área {area}m² precisa de {tinta_necessaria:.0f} litros de tinta para ser pintada inteira.')
+print(f'Your wall has dimensions {length}x{width} and its area is {area}m^2.')
+print(f'A wall with area {area}m^2 needs {paint_needed:.0f} liters of paint to be fully painted.')
