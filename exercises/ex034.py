df35608f260453f58c12b5f9ae53b57407a079f2
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 21:28:02 -0300

    Update ex034.py

--- exercises/ex034.py
+++ exercises/ex034.py
@@ -1,16 +1,16 @@
-peso = float(input('Digite o seu peso (kg): '))
-altura = float(input('Digite a sua altura (cm): '))
+weight = float(input('Enter your weight (kg): '))
+height_cm = float(input('Enter your height (cm): '))
 
-altura = altura / 100
-imc = peso / (altura ** 2)
+height_m = height_cm / 100
+bmi = weight / (height_m ** 2)
 
-if imc < 18.5:
-    print('Você está abaixo do peso.')
-elif imc >= 18.5 and imc < 25:
-    print('Você está no peso ideal.')
-elif imc >= 25 and imc < 30:
-    print('Você está acima do peso.')
-elif imc >= 30 and imc < 40:
-    print('Você está obeso.')
-elif imc >= 40:
-    print('Você está com obesidade mórbida.')
+if bmi < 18.5:
+    print('You are underweight.')
+elif bmi >= 18.5 and bmi < 25:
+    print('You are at an ideal weight.')
+elif bmi >= 25 and bmi < 30:
+    print('You are overweight.')
+elif bmi >= 30 and bmi < 40:
+    print('You are obese.')
+elif bmi >= 40:
+    print('You have morbid obesity.')
