5ed72ff505cff786277548e62271efe9e875bdec
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 21:27:55 -0300

    Update ex031.py

--- exercises/ex031.py
+++ exercises/ex031.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 from time import sleep
 
-ano_nascimento = int(input('Digite o seu ano de nascimento: '))
-idade = datetime.today().year - ano_nascimento
-print('Analisando a sua categoria de natação...')
+birth_year = int(input('Enter your birth year: '))
+age = datetime.today().year - birth_year
+print('Analyzing your swimming category...')
 sleep(3)
 
-if idade <= 9:
-    print('A sua categoria é a categoria mirim.')
-elif idade <= 14:
-    print('A sua categoria é a categoria infantil.')
-elif idade <= 19:
-    print('A sua categoria é a categoria junior.')
-elif idade <= 25:
-    print('A sua categoria é a categoria senior.')
-elif idade > 25:
-    print('A sua categoria é a categoria master.')
+if age <= 9:
+    print('Your category is the youth category.')
+elif age <= 14:
+    print('Your category is the child category.')
+elif age <= 19:
+    print('Your category is the junior category.')
+elif age <= 25:
+    print('Your category is the senior category.')
+elif age > 25:
+    print('Your category is the master category.')
