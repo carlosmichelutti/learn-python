b60a9994c4c7b61d5b3a8e9645d5507c6a8f4482
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 21:33:45 -0300

    Update ex050.py

--- exercises/ex050.py
+++ exercises/ex050.py
@@ -1,12 +1,12 @@
-quantidade_numeros = 0
-numero = 0
-soma = 0
+number_count = 0
+number = 0
+total = 0
 while True:
-    numero = int(input('Digite um número inteiro qualquer [999 para parar]: '))
-    if numero == 999:
+    number = int(input('Enter any integer [999 to stop]: '))
+    if number == 999:
         break
-    soma += numero
-    quantidade_numeros += 1
+    total += number
+    number_count += 1
 
-print(f'A soma de todos os {quantidade_numeros} números digitados é igual a {soma}.')
-print(f'A média de todos os {quantidade_numeros} números digitados é igual a {soma / quantidade_numeros}.')
+print(f'The sum of all {number_count} numbers entered is {total}.')
+print(f'The average of all {number_count} numbers entered is {total / number_count}.')
