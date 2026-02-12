3be1a068f00ae2dfca4f1b02d10ef5ea7326edc4
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-11 21:27:57 -0300

    Update ex032.py

--- exercises/ex032.py
+++ exercises/ex032.py
@@ -1,12 +1,12 @@
-nome = str(input('Digite o nome do aluno: '))
-nota1 = float(input(f'Digite a primeira nota do aluno {nome}: '))
-nota2 = float(input(f'Digite a segunda nota do aluno {nome}: '))
+student_name = str(input('Enter the student name: '))
+grade1 = float(input(f'Enter the first grade for {student_name}: '))
+grade2 = float(input(f'Enter the second grade for {student_name}: '))
 
-media = float((nota1 + nota2) / 2)
+average = float((grade1 + grade2) / 2)
 
-if media < 5.0:
-    print('Reprovado! A sua média foi abaixo da requerida.')
-elif media > 5.0 and media < 6.9:
-    print('Recuperação! A sua nota está razoável. Recupere-a para passar de ano.')
-elif media >= 7.0:
-    print('Parabéns! Você foi aprovado e passou de ano! Até o ano que vem!')
+if average < 5.0:
+    print('Failed! Your average was below the required level.')
+elif average > 5.0 and average < 6.9:
+    print('Recovery! Your grade is reasonable. Improve it to pass the year.')
+elif average >= 7.0:
+    print('Congratulations! You passed and moved on to the next year! See you next year!')
