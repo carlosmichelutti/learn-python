978509824af42aa87277ba7615da12cb0bf2cb52
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-12 21:30:34 -0300

    Update ex067.py

--- exercises/ex067.py
+++ exercises/ex067.py
@@ -1,37 +1,37 @@
-numeros_matriz = [
+matrix_numbers = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ]
 
-soma_terceira_coluna = 0
-maior = 0
-soma = 0 
+third_column_sum = 0
+highest = 0
+even_sum = 0
 
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        numeros_matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
+for row in range(0, 3):
+    for column in range(0, 3):
+        matrix_numbers[row][column] = int(input(f'Enter a value for [{row}, {column}]: '))
 
-for linha in range (0, 3):
-    for coluna in range(0, 3):
-        if numeros_matriz[linha][coluna] % 2 == 0:
-            soma = soma + numeros_matriz[linha][coluna]
+for row in range(0, 3):
+    for column in range(0, 3):
+        if matrix_numbers[row][column] % 2 == 0:
+            even_sum = even_sum + matrix_numbers[row][column]
 
-for linha in range(0, 3):
-    soma_terceira_coluna = soma_terceira_coluna + numeros_matriz[linha][2]
+for row in range(0, 3):
+    third_column_sum = third_column_sum + matrix_numbers[row][2]
 
-for coluna in range(0, 3):
-    if coluna == 0:
-        maior = numeros_matriz[1][coluna]
+for column in range(0, 3):
+    if column == 0:
+        highest = matrix_numbers[1][column]
 
-    elif numeros_matriz[1][coluna] > maior:
-        maior = numeros_matriz[1][coluna]
+    elif matrix_numbers[1][column] > highest:
+        highest = matrix_numbers[1][column]
 
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        print(f'[{numeros_matriz[linha][coluna]:^5}]', end=' ')
+for row in range(0, 3):
+    for column in range(0, 3):
+        print(f'[{matrix_numbers[row][column]:^5}]', end=' ')
     print('')
 
-print(f'A soma dos números pares da matriz é igual a {soma}.')
-print(f'A soma dos números da 3 coluna é igual a {soma_terceira_coluna}.')
-print(f'O maior valor da segunda linha foi o número {maior}.')
+print(f'The sum of even numbers in the matrix is {even_sum}.')
+print(f'The sum of numbers in the 3rd column is {third_column_sum}.')
+print(f'The highest value in the second row was {highest}.')
