cca80f44ee13a00df11413f825ba9db3a03e7b3b
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-12 21:30:27 -0300

    Update ex063.py

--- exercises/ex063.py
+++ exercises/ex063.py
@@ -1,35 +1,35 @@
-auxiliar = []
-grupo = []
-maior = 0
-menor = 0
+helper = []
+group = []
+highest = 0
+lowest = 0
 while True:
-    auxiliar.append(str(input('Nome: ')))
-    auxiliar.append(float(input('Peso (KG): ')))
-    if len(grupo) == 0:
-        maior = auxiliar[1]
-        menor = auxiliar[1]
+    helper.append(str(input('Name: ')))
+    helper.append(float(input('Weight (KG): ')))
+    if len(group) == 0:
+        highest = helper[1]
+        lowest = helper[1]
     else:
-        if auxiliar[1] > maior:
-            maior = auxiliar[1]
-        if auxiliar[1] < menor:
-            menor = auxiliar[1]
+        if helper[1] > highest:
+            highest = helper[1]
+        if helper[1] < lowest:
+            lowest = helper[1]
 
-    grupo.append(auxiliar[:])
-    auxiliar.clear()
-    resposta = str(input('Quer continuar? [S/N]: ')).upper().strip()
-    if resposta == 'N':
-        print(f'Ao todo foram registradas {len(grupo)} pessoas.')
-        print(f'O maior peso foi de {maior} kg, foi de ', end='')
-        for pessoa in grupo:
-            if pessoa[1] == maior:
-                print(pessoa[0], end='.\n')
+    group.append(helper[:])
+    helper.clear()
+    answer = str(input('Do you want to continue? [Y/N]: ')).upper().strip()
+    if answer == 'N':
+        print(f'A total of {len(group)} people were registered.')
+        print(f'The highest weight was {highest} kg, belonging to ', end='')
+        for person in group:
+            if person[1] == highest:
+                print(person[0], end='.\n')
 
-        print(f'O menor peso digitado foi de {menor} kg, foi de ', end='')
-        for pessoa in grupo:
-            if pessoa[1] == menor:
-                print(pessoa[0], end='.\n')
+        print(f'The lowest weight entered was {lowest} kg, belonging to ', end='')
+        for person in group:
+            if person[1] == lowest:
+                print(person[0], end='.\n')
         break
 
-    elif resposta != 'S':
-        print('Resposta inválida. Tente novamente: ')
-        resposta = str(input('Quer continuar? [S/N]: ')).upper().strip()
+    elif answer != 'Y':
+        print('Invalid response. Try again: ')
+        answer = str(input('Do you want to continue? [Y/N]: ')).upper().strip()
