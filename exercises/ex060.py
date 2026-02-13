13f07a66b1f9d4cafcda8bbca69ee9e949f9c0bd
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-12 21:30:19 -0300

    Update ex060.py

--- exercises/ex060.py
+++ exercises/ex060.py
@@ -1,19 +1,19 @@
-numeros_em_ordem = []
+numbers_in_order = []
 for num in range(0, 5):
-    numero = int(input('Digite um número qualquer: '))
-    if num == 0 or numero > numeros_em_ordem[-1]:
-        numeros_em_ordem.append(numero)
+    number = int(input('Enter any number: '))
+    if num == 0 or number > numbers_in_order[-1]:
+        numbers_in_order.append(number)
     else:
         position = 0
-        while position < len(numeros_em_ordem):
-            if numero <= numeros_em_ordem[position]:
-                numeros_em_ordem.insert(position, numero)
+        while position < len(numbers_in_order):
+            if number <= numbers_in_order[position]:
+                numbers_in_order.insert(position, number)
                 break
             position += 1
 
-    if numero > 0 and numero < 6:
-        print('Adicionando no começo da lista...')
-    elif numero > 6:
-        print('Adicionando no final da lista...')
+    if number > 0 and number < 6:
+        print('Adding to the beginning of the list...')
+    elif number > 6:
+        print('Adding to the end of the list...')
 
-print(f'A lista em ordem é essa: {numeros_em_ordem}')
+print(f'The list in order is: {numbers_in_order}')
