4cbddc038a2b864fbc019304fcd8aedf54c38f1c
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:50 -0300

    Update ex035.py

--- exercises/ex035.py
+++ exercises/ex035.py
@@ -1,4 +1,4 @@
-price = float(input('What is the normal price of the product? R$'))
+price = float(input('What is the normal price of the product? $'))
 
 print('What is the payment condition? ')
 print('[1] Cash/check (upfront)')
@@ -8,21 +8,21 @@ print('[4] Card in 3 or more installments')
 
 payment_condition = int(input('Enter an option: '))
 if payment_condition == 1:
-    print(f'The product price gets a 10% discount and the new amount is R${(price * 0.90):.2f}.')
+    print(f'The product price gets a 10% discount and the new amount is ${(price * 0.90):.2f}.')
 
 elif payment_condition == 2:
-    print(f'The product price gets a 5% discount and the new amount is R${(price * 0.95):.2f}.')
+    print(f'The product price gets a 5% discount and the new amount is ${(price * 0.95):.2f}.')
 
 elif payment_condition == 3:
     total_installments = 2
     print(
-        f'The product price will be R${(price * 0.95):.2f}, '
-        f'in {total_installments} installments of R${((price * 0.95) / total_installments):.2f} each.'
+        f'The product price will be ${(price * 0.95):.2f}, '
+        f'in {total_installments} installments of ${((price * 0.95) / total_installments):.2f} each.'
     )
 
 elif payment_condition == 4:
     total_installments = int(input('How many installments will you use? '))
     print(
-        f'The product price will be R${(price * 0.95):.2f}, '
-        f'in {total_installments} installments of R${((price * 0.95) / total_installments):.2f} each.'
+        f'The product price will be ${(price * 0.95):.2f}, '
+        f'in {total_installments} installments of ${((price * 0.95) / total_installments):.2f} each.'
     )
