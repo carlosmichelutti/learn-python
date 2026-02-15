0411c188225649845c8654bb8ce5f8b006d057e7
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:14 -0300

    Update ex008.py

--- exercises/ex008.py
+++ exercises/ex008.py
@@ -1,5 +1,5 @@
-price = float(input('What is the product price? R$'))
+price = float(input('What is the product price? $'))
 
 discounted_price = price * 0.9
 
-print(f'The product that cost R${price:.2f} with a 10% discount will cost R${discounted_price:.2f}.')
+print(f'The product that cost ${price:.2f} with a 10% discount will cost ${discounted_price:.2f}.')
