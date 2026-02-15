5c146b14de8c3580ff28ea9b25f32499cedc5e27
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:30 -0300

    Update ex021.py

--- exercises/ex021.py
+++ exercises/ex021.py
@@ -1,10 +1,10 @@
 name = str(input('Enter your name: '))
 
-salary = float(input(f'{name}, enter your current salary in R$: '))
+salary = float(input(f'{name}, enter your current salary in $: '))
 
 if salary > 1250.00:
     raise_amount = salary * (10 / 100)
-    print(f'Your previous salary was R${salary:.2f}, and your current salary is R${(salary + raise_amount):.2f}.')
+    print(f'Your previous salary was ${salary:.2f}, and your current salary is ${(salary + raise_amount):.2f}.')
 else:
     raise_amount = salary * (15 / 100)
-    print(f'Your previous salary was R${salary:.2f}, and your current salary is R${(salary + raise_amount):.2f}.')
+    print(f'Your previous salary was ${salary:.2f}, and your current salary is ${(salary + raise_amount):.2f}.')
