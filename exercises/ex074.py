588a7b5316dfe8e0360f3bf71cc68eb52543426b
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 15:40:37 -0300

    Update ex074.py

--- exercises/ex074.py
+++ exercises/ex074.py
@@ -1,10 +1,10 @@
-def verificar_maior_valor(numeros: list):
-    maior_valor = 0
-    for numero in numeros:
-        if numero > maior_valor:
-            maior_valor = numero
-    print(f'O maior valor encontrado na lista {numeros} foi o número {maior_valor}.')
+def find_largest_value(numbers: list):
+    largest_value = 0
+    for number in numbers:
+        if number > largest_value:
+            largest_value = number
+    print(f'The largest value found in the list {numbers} was {largest_value}.')
     
-verificar_maior_valor(numeros=[4, 5, 6, 9, 15])
-verificar_maior_valor(numeros=[1, 4, 5, 7, 3])
-verificar_maior_valor(numeros=[1, 10])
+find_largest_value(numbers=[4, 5, 6, 9, 15])
+find_largest_value(numbers=[1, 4, 5, 7, 3])
+find_largest_value(numbers=[1, 10])
