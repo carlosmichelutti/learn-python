828ed13c0b4aba8bfd2b7277af15fb8034758cdd
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:24 -0300

    Update ex016.py

--- exercises/ex016.py
+++ exercises/ex016.py
@@ -3,4 +3,4 @@ kilometers = int(input('How many kilometers driven? '))
 
 total_cost = (60 * days) + (kilometers * 0.15)
 
-print(f'The total cost to pay when returning the car is R${total_cost:.2f}.')
+print(f'The total cost to pay when returning the car is ${total_cost:.2f}.')
