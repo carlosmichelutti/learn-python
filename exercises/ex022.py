91bb47eff35b14a13cc6967ed0af51ae97b61313
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:34 -0300

    Update ex022.py

--- exercises/ex022.py
+++ exercises/ex022.py
@@ -2,6 +2,6 @@ speed = int(input('Enter the car speed in km/h: '))
 
 if speed > 80:
     fine_amount = (speed - 80) * 7
-    print(f'You exceeded the maximum speed by {speed - 80} km/h on the highway. Your fine is R${fine_amount}.')
+    print(f'You exceeded the maximum speed by {speed - 80} km/h on the highway. Your fine is ${fine_amount}.')
 else:
     print('Congratulations, your speed is within the highway limit. Keep it up!')
