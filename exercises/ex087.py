6f9b92d1f50f3d9dc23dc7b61c1c294cb1bbedf8
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 15:41:05 -0300

    Update ex087.py

--- exercises/ex087.py
+++ exercises/ex087.py
@@ -1,7 +1,7 @@
 from random import randint
 
-def mapear(function, list):
-    return (function(elemento) for elemento in list)
+def map_function(function, items):
+    return (function(element) for element in items)
 
-print(list(mapear(lambda x: x ** 2, [randint(1, 10) for _ in range(randint(1, 10))])))
-print(list(mapear(lambda x: x * 2, [randint(1, 10) for _ in range(randint(1, 10))])))
+print(list(map_function(lambda x: x ** 2, [randint(1, 10) for _ in range(randint(1, 10))])))
+print(list(map_function(lambda x: x * 2, [randint(1, 10) for _ in range(randint(1, 10))])))
