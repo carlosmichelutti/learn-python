273b5e6d882741263ed5cc1a9ce88928fd2574ce
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 15:41:11 -0300

    Update ex090.py

--- exercises/ex090.py
+++ exercises/ex090.py
@@ -1,36 +1,36 @@
-def calcular_minimo_divisor_comum(numeros: list[int]) -> None:
+def calculate_greatest_common_divisor(numbers: list[int]) -> None:
 
-    menor_numero = min(numeros)
-    lista_de_mdcs = []
+    smallest_number = min(numbers)
+    gcd_list = []
     while True:
-        divisor = 0
-        for numero in numeros:
-            if menor_numero == 0:
-                print('Essa sequencia númerica não tem MDC.')
+        divisor_count = 0
+        for number in numbers:
+            if smallest_number == 0:
+                print('This numeric sequence has no GCD.')
                 break
 
-            if numero % menor_numero == 0:
-                divisor += 1
-                lista_de_mdcs.append(menor_numero)
+            if number % smallest_number == 0:
+                divisor_count += 1
+                gcd_list.append(smallest_number)
 
-        if divisor == len(numeros):
-            for divisor in range(len(lista_de_mdcs)):
-                if lista_de_mdcs[divisor]:
-                    menor_divisor = lista_de_mdcs[divisor]
-                elif divisor > menor_divisor and divisor > 1:
-                    menor_divisor = lista_de_mdcs[divisor]
+        if divisor_count == len(numbers):
+            for divisor in range(len(gcd_list)):
+                if gcd_list[divisor]:
+                    smallest_divisor = gcd_list[divisor]
+                elif divisor > smallest_divisor and divisor > 1:
+                    smallest_divisor = gcd_list[divisor]
                 else:
-                    menor_divisor = 1
+                    smallest_divisor = 1
 
-            print(f'O MDC dos números {numeros} é {menor_divisor}')
+            print(f'The GCD of the numbers {numbers} is {smallest_divisor}')
             break
 
-        menor_numero -= 1
+        smallest_number -= 1
 
-calcular_minimo_divisor_comum([9, 3, 5])
-calcular_minimo_divisor_comum([123, 100, 5])
-calcular_minimo_divisor_comum([25, 5, 15])
-calcular_minimo_divisor_comum([18,60])
-calcular_minimo_divisor_comum([55, 22])
-calcular_minimo_divisor_comum([15, 150])
-calcular_minimo_divisor_comum([81, 9])
+calculate_greatest_common_divisor([9, 3, 5])
+calculate_greatest_common_divisor([123, 100, 5])
+calculate_greatest_common_divisor([25, 5, 15])
+calculate_greatest_common_divisor([18,60])
+calculate_greatest_common_divisor([55, 22])
+calculate_greatest_common_divisor([15, 150])
+calculate_greatest_common_divisor([81, 9])
