f4ab9049b5807563aaa70f6db526afd3d83528bd
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 15:40:52 -0300

    Update ex081.py

--- exercises/ex081.py
+++ exercises/ex081.py
@@ -1,16 +1,16 @@
 from functools import reduce
 from random import randint
 
-pessoas = [
-    {'nome': 'Mariana', 'idade': randint(10, 99)},
-    {'nome': 'Carlos', 'idade': randint(10, 99)},
-    {'nome': 'Arthur', 'idade': randint(10, 99)},
-    {'nome': 'Rebeca', 'idade': randint(10, 99)},
-    {'nome': 'Rodolfo', 'idade': randint(10, 99)},
-    {'nome': 'Maria', 'idade': randint(10, 99)}
+people = [
+    {'name': 'Mariana', 'age': randint(10, 99)},
+    {'name': 'Carlos', 'age': randint(10, 99)},
+    {'name': 'Arthur', 'age': randint(10, 99)},
+    {'name': 'Rebeca', 'age': randint(10, 99)},
+    {'name': 'Rodolfo', 'age': randint(10, 99)},
+    {'name': 'Maria', 'age': randint(10, 99)}
 ]
 
-idades = list(map(lambda pessoa: pessoa['idade'], pessoas))
-maiores_de_idade = list(filter(lambda pessoa: pessoa > 18, idades))
-soma_idades = reduce(lambda soma_idades, idade: soma_idades + idade, maiores_de_idade, 0)
-print(f'A soma das idades das pessoas maiores de idade é igual a {soma_idades}.')
+ages = list(map(lambda person: person['age'], people))
+adult_ages = list(filter(lambda age: age > 18, ages))
+age_sum = reduce(lambda total, age: total + age, adult_ages, 0)
+print(f'The sum of the ages of adults is {age_sum}.')
