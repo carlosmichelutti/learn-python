ca93e244c72cf3dd6ff2150b1ae71231a8883d22
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 16:38:43 -0300

    Update ex092.py

--- exercises/ex092.py
+++ exercises/ex092.py
@@ -1,20 +1,46 @@
+from time import sleep
+
 class Car:
     def __init__(
         self: object,
         model: str,
         year: int,
-        color: str
+        color: str,
+        maximum_speed: int = 120,
+        initial_velocity: int = 0
     ) -> None:
 
         self.model: str = model
         self.year: int = year
         self.color: str = color
+        self.maximum_speed: int = maximum_speed
+        self.initial_velocity: int = initial_velocity
 
     def __str__(self):
         return f'Model: {self.model} | Year: {self.year} | Color: {self.color}'
 
-car1 = Car(model='Toyota Camry', year=2020, color='Red')
-car2 = Car(model='Honda Accord', year=2019, color='Blue')
-car3 = Car(model='Ford Mustang', year=2021, color='Black')
+    def accelerate(self: object, acceleration: int) -> None:
+
+        while not self.initial_velocity >= self.maximum_speed:
+            print(f'{self.model} accelerating: {self.initial_velocity} km/h')
+            self.initial_velocity += acceleration
+            sleep(0.5)
+        
+        print(f'{self.model} reached maximum speed: {self.initial_velocity} km/h')
+
+    def brake(self: object, deceleration: int) -> None:
+
+        while not self.initial_velocity <= 0:
+            print(f'{self.model} braking: {self.initial_velocity} km/h')
+            self.initial_velocity -= deceleration
+            sleep(0.5)
+        
+        print(f'{self.model} stopped.')
+
+car1 = Car(model='Toyota Camry', year=2020, color='Red', maximum_speed=150)
+car2 = Car(model='Honda Accord', year=2019, color='Blue', maximum_speed=140)
+car3 = Car(model='Ford Mustang', year=2021, color='Black', maximum_speed=200)
+car3.accelerate(acceleration=10)
+car3.brake(deceleration=5)
 
 print(car1, car2, car3, sep='\n')
