15edd60b56e60c2d7d31b4b51896444175fa3524
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:40 -0300

    Update ex023.py

--- exercises/ex023.py
+++ exercises/ex023.py
@@ -1,6 +1,6 @@
 distance = int(input('How many km will you travel? '))
 
 if distance <= 200:
-    print(f'The trip cost is R${(distance * 0.50):.2f}.')
+    print(f'The trip cost is ${(distance * 0.50):.2f}.')
 else:
-    print(f'The total trip cost will be R${(distance * 0.45):.2f}.')
+    print(f'The total trip cost will be ${(distance * 0.45):.2f}.')
