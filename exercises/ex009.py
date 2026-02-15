01b1df0cea8e2d617cd2dc2745d847940ffff1d2
Carlos Michelutti <carlos.stz590@gmail.com>
2026-02-15 14:29:20 -0300

    Update ex009.py

--- exercises/ex009.py
+++ exercises/ex009.py
@@ -1,5 +1,5 @@
-salary = float(input('What is your salary? R$'))
+salary = float(input('What is your salary? $'))
 
 raise_amount = salary * 1.15
 
-print(f'A person who earns R${salary} with a 15% raise will now earn R${raise_amount:.2f}.')
+print(f'A person who earns ${salary} with a 15% raise will now earn ${raise_amount:.2f}.')
